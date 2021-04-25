4d3d9eaaa62fa5e451f0c2b06fe78213a0206738
Dinar <sagitoff.dinar@yandex.ru>
2021-04-25 15:03:24 +0300

    Домашнее задание к занятию 2.2 «Прототип и конструктор объекта»

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,40 @@
 //String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function () {
+    let strToLowerCase = this.toLowerCase();  
+    const len = Math.floor(strToLowerCase.length / 2);
+    for (let i = 0; i < len; i++) {
+        if (strToLowerCase[i] !== strToLowerCase[strToLowerCase.length - i - 1]){
+            return false;
+        }
+        return true;    
+    }
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+
+// Задача №2
+function getAverageMark(marks) {
+    if (marks.length === 0){
+        return 0;
+    }
+    let result = marks.reduce(function(marks, current) {
+        return marks + current;
+     }, 0);
+     let averageMark = result / marks.length
+     return Math.round(averageMark);
 }
+
+
+// Задача №3
+function checkBirthday(birthday) {
+    let now = Date.now();
+    let birthdayDate = (now - Date.parse(birthday));
+    let diff = Math.floor((birthdayDate / 86400000) / 365.2468);
+    if (diff > 18) {
+      return diff;
+    }
+    
+}
+
+
